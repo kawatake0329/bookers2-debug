bf5551d7a4274c4c5a602c8daace0cf2a4139aa4
kawatake0329 <104962638+kawatake0329@users.noreply.github.com>
2022-07-08 01:44:56 +0000

    [finish]task4

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,12 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get "home/about"=>"homes#about"
 
+  resources :users do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
+
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
